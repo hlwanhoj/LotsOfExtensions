c26943dea44fc83ea6b4bf64035a3b8f3d800248
hlwanhoj <hlwanhoj@gmail.com>
2021-04-01 13:35:52 +0800

    Group `UIEdgeInsets` extensions. Rename files

--- LotsOfExtensions/Classes/Foundation/CGSize.swift
+++ LotsOfExtensions/Classes/Foundation/CGSize.swift
@@ -7,26 +7,28 @@
 
 import Foundation
 
+// MARK: - Math
+    
 public extension CGSize {
-	static prefix func - (left: CGSize) -> CGSize {
+	static prefix func - (left: Self) -> Self {
 		return CGSize(width: -left.width, height: -left.height)
 	}
 	
 	// Interact with CGFloat
 	
-	static func + (left: CGSize, right: CGFloat) -> CGSize {
+	static func + (left: Self, right: CGFloat) -> Self {
 		  return CGSize(width: left.width + right, height: left.height + right)
 	}
 
-	static func - (left: CGSize, right: CGFloat) -> CGSize {
+	static func - (left: Self, right: CGFloat) -> Self {
 		  return left + (-right)
 	}
 
-	static func * (left: CGSize, right: CGFloat) -> CGSize {
+	static func * (left: Self, right: CGFloat) -> Self {
 		  return CGSize(width: left.width * right, height: left.height * right)
 	}
 
-	static func / (left: CGSize, right: CGFloat) -> CGSize {
+	static func / (left: Self, right: CGFloat) -> Self {
 		return left * (1.0 / right)
 	}
 }
--- LotsOfExtensions/Classes/UIKit/UIEdgeInsets.swift
+++ LotsOfExtensions/Classes/UIKit/UIEdgeInsets.swift
@@ -12,17 +12,17 @@ public extension UIEdgeInsets {
 	// Taking [`EdgeInsets`](https://api.flutter.dev/flutter/painting/EdgeInsets-class.html) in Flutter as reference
 	
 	/// Creates insets where all the offsets are value.
-	static func all(value: CGFloat) -> UIEdgeInsets {
+	static func all(value: CGFloat) -> Self {
 		return .init(top: value, left: value, bottom: value, right: value)
 	}
 	
 	/// Creates insets with only the given values.
-	static func only(top: CGFloat = 0, left: CGFloat = 0, bottom: CGFloat = 0, right: CGFloat = 0) -> UIEdgeInsets {
+	static func only(top: CGFloat = 0, left: CGFloat = 0, bottom: CGFloat = 0, right: CGFloat = 0) -> Self {
 		return .init(top: top, left: left, bottom: bottom, right: right)
 	}
 
 	///	Creates insets with symmetrical vertical and horizontal offsets.
-	static func symmetric(vertical: CGFloat = 0, horizontal: CGFloat = 0) -> UIEdgeInsets {
+	static func symmetric(vertical: CGFloat = 0, horizontal: CGFloat = 0) -> Self {
 		return .init(top: vertical, left: horizontal, bottom: vertical, right: horizontal)
 	}
 
@@ -33,5 +33,12 @@ public extension UIEdgeInsets {
 	
 	/// Sum of all vertical values
 	var verticals: CGFloat { top + bottom }
-	
+}
+
+// MARK: - Math
+
+public extension UIEdgeInsets {
+    static prefix func - (val: Self) -> Self {
+        return UIEdgeInsets(top: -val.top, left: -val.left, bottom: -val.bottom, right: -val.right)
+    }
 }
